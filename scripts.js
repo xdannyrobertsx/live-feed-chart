d2b3f180dc8b6b09bb7a9d358997276968828338
danny <64439795+xdannyrobertsx@users.noreply.github.com>
2023-02-07 12:49:53 -0600

    new logic

--- scripts.js
+++ scripts.js
@@ -4,11 +4,13 @@ const newChart = () => {
   liveFeedChart.destroy();
 
   liveFeedChart = new Chart(myChart, userOptions);
+  
+  liveFeedChart.canvas.parentNode.style.width = '1200px';
 };
 
 // data fetch and cleanup
 const url =
-  "https://thrillshare-cmsv2.services.thrillshare.com/api/v2/s/108979/live_feeds?page_size=100";
+  "https://thrillshare-cmsv2.services.thrillshare.com/api/v2/s/108979/live_feeds?page_size=200";
 
 const fetchData = async (endpoint) => {
   const res = await fetch(endpoint);
@@ -16,13 +18,19 @@ const fetchData = async (endpoint) => {
 };
 
 const cleanData = (rawData) => {
+  console.log(rawData)
   let lf_cleanData = {};
+  const currentYear = new Date().getFullYear()
   rawData.forEach((post) => {
-    if (lf_cleanData[post.author_name]) {
-      lf_cleanData[post.author_name] += 1;
-    } else {
-      lf_cleanData[post.author_name] = 1;
+    let postYear = parseInt(post.time.split("-")[0])
+    if (currentYear == postYear) {
+       if (lf_cleanData[post.author_name]) {
+        lf_cleanData[post.author_name] += 1;
+      } else {
+        lf_cleanData[post.author_name] = 1;
+      }
     }
+
   });
   let sortedArray = Object.entries(lf_cleanData).sort((a, b) => b[1] - a[1]);
   let lf_cleanData_sorted = Object.fromEntries(sortedArray);
@@ -48,7 +56,7 @@ document.querySelector("#chartTypes").addEventListener("change", () => {
 });
 
 Chart.defaults.global.defaultFontFamily = "Lato";
-Chart.defaults.global.defaultFontSize = 12;
+Chart.defaults.global.defaultFontSize = 14;
 Chart.defaults.global.defaultFontColor = "#777";
 
 let myChart = document.getElementById("myChart").getContext("2d");
@@ -74,7 +82,7 @@ let userOptions = {
   options: {
     title: {
       display: true,
-      text: "Largest Cities In Massachusetts",
+      text: "Live Feed Posts",
       fontSize: 25,
     },
     legend: {
@@ -86,16 +94,18 @@ let userOptions = {
     },
     layout: {
       padding: {
-        left: 50,
+        left: 0,
         right: 0,
         bottom: 0,
         top: 0,
       },
+      
     },
     tooltips: {
       enabled: true,
     },
     responsive: true,
+    maintainAspectRatio: true,
     scales: {
       yAxes: [{
         ticks: {
